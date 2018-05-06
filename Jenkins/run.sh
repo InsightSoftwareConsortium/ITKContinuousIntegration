7ddc34d9e6c32faf3b36e108c4aa06be15516579
Matt McCormick <matt.mccormick@kitware.com>
2018-05-06 18:45:35 -0400

    BUG: Work around hyper connection issue
    As indicated in the Jenkins logs and as suggested here:
    
      https://support.cloudbees.com/hc/en-us/articles/209715698-How-to-add-Java-arguments-to-Jenkins-
    
      https://issues.jenkins-ci.org/browse/JENKINS-41384

--- Jenkins/run.sh
+++ Jenkins/run.sh
@@ -29,6 +29,6 @@ jnlp_port=50000
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --restart unless-stopped -p $jnlp_port:$jnlp_port -p 8080:8080 -v $HOME/itkjenkins:/var/jenkins_home --name $jenkins_container_name -d jenkins
+docker run --restart unless-stopped -p $jnlp_port:$jnlp_port -p 8080:8080 -v $HOME/itkjenkins:/var/jenkins_home -e "JAVA_OPTS=-Djenkins.slaves.DefaultJnlpSlaveReceiver.disableStrictVerification=true" --name $jenkins_container_name -d jenkins
 
 docker run -p 443:443 -p 80:80 -v $SSL_CERT:/etc/ssl/your_domain_name.pem -v $SSL_KEY:/etc/ssl/your_domain_name.key -v $DIR/nginx.conf:/etc/nginx/conf.d/default.conf --name $nginx_container_name --link itkjenkins:itkjenkins -d nginx
